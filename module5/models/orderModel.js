2fad910bad19e638914c734b2f3a0c233039a7db
Android Developer <geneh@shaw.ca>
2022-02-27 12:09:31 +0530

    module5

---
+++ module5/models/orderModel.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose')
+Schema = mongoose.Schema
+
+var orderModel = new Schema({
+    name: {type:String},
+    email: {type:String},
+    address: {type:String},
+    product_name: {type:String},
+    unit_price: {type:String},
+    quantity: {type:String},
+    order_date: {type:String}
+})
+
+
+module.exports = mongoose.model('orderlist', orderModel, 'orderlist')
